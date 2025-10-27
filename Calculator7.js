9085e9a099f98631e35acf9b214cc01f2b788a7a
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:50:42 +0800

    Divide function

--- Calculator7.js
+++ Calculator7.js
@@ -10,8 +10,13 @@ function multiply(a, b){
     return a*b
 }
 
+function divide(a,b){
+    return a/b
+}
+
 let a = 5
 let b = 3
 console.log("ADDITION: ", add(a, b))
 console.log("SUBTRACT: ", subtract(a, b))
 console.log("MULTIPLICATION: ", multiply(a,b))
+console.log("DIVIDE: ", divide(a,b))
