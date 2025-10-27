480144d5882e086e05f1a0be1f7f581940f47a54
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:49:12 +0800

    Subtract function

--- Calculator7.js
+++ Calculator7.js
@@ -2,6 +2,11 @@ function add(a,b){
     return a+b
 }
 
+function subtract(a,b){
+    return a-b
+}
+
 let a = 5
 let b = 3
 console.log("ADDITION: ", add(a, b))
+console.log("SUBTRACT: ", subtract(a, b))
