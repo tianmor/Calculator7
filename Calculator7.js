77d4c93a772755c47aee039dc4f1b8b2be05450c
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:53:55 +0800

    Remove Add function

--- Calculator7.js
+++ Calculator7.js
@@ -1,7 +1,3 @@
-function add(a,b){
-    return a+b
-}
-
 function subtract(a,b){
     return a-b
 }
@@ -16,7 +12,6 @@ function divide(a,b){
 
 let a = 5
 let b = 3
-console.log("ADDITION: ", add(a, b))
 console.log("SUBTRACT: ", subtract(a, b))
 console.log("MULTIPLICATION: ", multiply(a,b))
 console.log("DIVIDE: ", divide(a,b))
