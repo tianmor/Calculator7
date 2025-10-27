fd6822d8bae142043a78d9f8f8751535d62c596a
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:50:04 +0800

    Multiply function

--- Calculator7.js
+++ Calculator7.js
@@ -6,7 +6,12 @@ function subtract(a,b){
     return a-b
 }
 
+function multiply(a, b){
+    return a*b
+}
+
 let a = 5
 let b = 3
 console.log("ADDITION: ", add(a, b))
 console.log("SUBTRACT: ", subtract(a, b))
+console.log("MULTIPLICATION: ", multiply(a,b))
